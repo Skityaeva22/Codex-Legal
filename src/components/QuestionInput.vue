6de0517535a194c35903283fb65e2946018c015c
a.skitiaeva <a.skitiaeva@bars-open.ru>
2025-08-19 15:56:05 +0500

    Добавление уникального ключа при загрузке файла

--- src/components/QuestionInput.vue
+++ src/components/QuestionInput.vue
@@ -79,6 +79,7 @@
             </ElButton>
           </ElTooltip>
           <input
+              :key="key"
               type="file"
               ref="fileInput"
               :accept="acceptedFormats"
@@ -133,6 +134,7 @@ const emit = defineEmits<{
 const input = ref("");
 const isValidFile = ref(false);
 const showFileList = ref(false);
+const key = ref("");
 
 const fileInput = ref<HTMLInputElement | null>(null);
 const acceptedFormats = '.doc,.docx,.pdf,.md,.xls,.xlsx,.epub,.fb2';
@@ -144,8 +146,20 @@ const isActiveSendButton = computed(() => {
   return input.value?.trim()?.length || files.value?.length;
 });
 
+
+function generateUniqueKey(length: number = 10): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let uniqueKey = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    uniqueKey += characters[randomIndex];
+  }
+  return uniqueKey;
+}
+
 const triggerUpload = () => {
   fileInput.value?.click();
+  key.value = generateUniqueKey(16);
 };
 
 const validateFileType = (file: File) => {
